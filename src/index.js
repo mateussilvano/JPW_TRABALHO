0d26cf755de16654eb773261ee20087e7e69ce96
Mateus Silvano <mateus_silvano@hotmail.com>
2021-10-20 21:36:39 -0300

    Confirmar em master

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 3000
 const routes = require('./api/routes/')
+const mongoose = require('mongoose')
+const url = 'mongodb+srv://sysdba:masterkey@cluster0.cwnax.mongodb.net/jpw?retryWrites=true&w=majority'
 
 app.use(cors())
 app.use(express.json())
 app.use('/api', routes)
 
-app.listen(port, function(){
-    console.log(`Servidor rodando na porta ${port}`)
+mongoose.connect(url).then(function(){
+    app.listen(8080, function(){
+        console.log(`Servidor rodando na porta ${porta}`)
+    
+    })
 })
